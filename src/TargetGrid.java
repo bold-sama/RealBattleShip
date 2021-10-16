95715383e806cd6d18e3bbd7a51c69340781bdca
brodys <brody.strock@gmail.com>
2021-10-16 17:17:02 -0600

    added the description of the grid to the TargetGrid, need to figure out logic for checking cellState

--- src/TargetGrid.java
+++ src/TargetGrid.java
@@ -1,12 +1,58 @@
-public class TargetGrid {
-    private Cell[][] cells = new Cell[10][10];
+import java.awt.*;
 
+public class TargetGrid {
+    Cell[][] cells;
     public TargetGrid(){
+        cells = new Cell[10][10];
         for(int x = 0; x < 10; x++){
             for (int y = 0; y <10; y++){
                 cells[x][y] = new Cell();
 
             }
         }
+    }
+
+    public void description(){
+        // print out console representation of the grid
+        printHeader();
+
+        for(int row = 0; row < 10; row++){
+            printRow(row);
+        }
+    }
+
+    private void printRow(int rowNumber){
+        char letter = (char)(rowNumber + 65);
+        System.out.print("| " + letter + " |");
+        for(int column = 0; column < 10; column++){
+            switch(cellAtPoint(new Point(column, rowNumber)).getState()){
+                case EMPTY -> System.out.print("   .");
+                case HIT -> System.out.print(" * .");
+                case MISS -> System.out.print(" o .");
+                case OCCUPIED -> System.out.print(" # .");
+            }
+        }
+        System.out.printf("%n");
+    }
+
+    private void printHeader(){
+        printBorder();
+        System.out.print("|   |");
+        for(int column = 1; column < 11; column++){
+            System.out.printf("%2d", column);
+            System.out.print(" |");
+        }
+        System.out.printf("%n");
+        printBorder();
+    }
+
+    private void printBorder(){
+        // blank column
+        System.out.print("+---+");
+        // letter columns
+        for(int column = 0; column < 10; column++){
+            System.out.print("---+");
+        }
+        System.out.printf("%n");
     }
 }
